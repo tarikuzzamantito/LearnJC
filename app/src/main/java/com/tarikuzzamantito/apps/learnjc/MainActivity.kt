6654ce7c2377b98dcabd475bd5aba31b7607a63b
tarik <tarikuzzaman1@gmail.com>
2025-03-09 14:42:22 +0600

    State Management

--- app/src/main/java/com/tarikuzzamantito/apps/learnjc/MainActivity.kt
+++ app/src/main/java/com/tarikuzzamantito/apps/learnjc/MainActivity.kt
@@ -4,25 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.tarikuzzamantito.apps.learnjc.delegateim.NetworkStateHandler
-import com.tarikuzzamantito.apps.learnjc.delegateim.NetworkStateListener
+import com.google.android.material.progressindicator.LinearProgressIndicator
 import com.tarikuzzamantito.apps.learnjc.ui.theme.LearnJCTheme
 
-class MainActivity : ComponentActivity(), NetworkStateListener by NetworkStateHandler() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            /*LearnJCTheme {
+            LearnJCTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
                 }
-            }*/
+            }
         }
-        observeNetworkState(this, this)
     }
 }
 
--- app/src/main/java/com/tarikuzzamantito/apps/learnjc/custom/CustomTextWidgets.kt
+++ app/src/main/java/com/tarikuzzamantito/apps/learnjc/custom/CustomTextWidgets.kt
@@ -1,15 +1,24 @@
 package com.tarikuzzamantito.apps.learnjc.custom
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -18,6 +27,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -62,9 +72,210 @@ fun CallComposable() {
         MyTextField()
 
         Counter()
+        StatefulCounter()
+        StatefulTextField()
+        // Stateful to Stateless 
+        var text by remember { mutableStateOf("") }
+        StatelessTextField(text = text, onValueChange = { newText ->
+            text = newText
+        })
+
+        // Call Challenge 2
+        var progress by remember { mutableStateOf(0.0f) }
+        Column(verticalArrangement = Arrangement.Center) {
+            LinearProgInd(progress = progress)
+
+            IncreaseProgressButton {
+                progress += 0.1f
+                if (progress > 1.0f) {
+                    // Reset the progress
+                    progress = 0.0f
+                }
+            }
+
+            ProgressText(progress = progress)
+        }
+
     }
 }
 
+
+/** Coding Challenge 2
+ * Create 3 Composables
+ * 1- Linear Progress Indicator:
+ * 2- Button: allows the user to increase the progress when being clicked
+ * 3- Text: Displaying the Progress
+ *
+ * Note: All these Composables should be stateless
+ */
+@Composable
+fun LinearProgInd(progress: Float) {
+    LinearProgressIndicator(progress = { progress })
+}
+
+@Composable
+fun IncreaseProgressButton(onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text("Increase Progress")
+    }
+}
+
+@Composable
+fun ProgressText(progress: Float) {
+    Text("The Progress is: $progress")
+}
+// ===============================================
+
+
+// 5- Progress Indicator: Represents the progress of a task
+// Circular Progress Indicator
+@Composable
+fun CircularProgInd() {
+    CircularProgressIndicator()
+    CircularProgressIndicator(progress = { 0.8f })
+}
+
+// Linear Progress Indicator
+@Composable
+fun LinearProInd() {
+    LinearProgressIndicator()
+    LinearProgressIndicator(progress = { 0.7f }, color = Color.Green, trackColor = Color.Gray)
+}
+// Determinate Progress Indicator
+//--------------------------------
+
+// Radio Button: users can select only one option from a list of options.
+@Composable
+fun RadioButtonExample() {
+    // 1- Tracking the currently selected option
+    var selectedOption by remember { mutableStateOf("Option 1") }
+
+    // 2- The Radio Buttons
+    Column {
+        RadioButtonRow(
+            text = "Option 1",
+            selectedOption == "Option 1",
+            onSelected = { selectedOption = "Option 1" },
+        )
+        RadioButtonRow(
+            text = "Option 2",
+            selectedOption == "Option 2",
+            onSelected = { selectedOption = "Option 2" },
+        )
+        RadioButtonRow(
+            text = "Option 3",
+            selectedOption == "Option 3",
+            onSelected = { selectedOption = "Option 3" },
+        )
+    }
+}
+
+// 4- Displaying a text beside Radio Button
+@Composable
+fun RadioButtonRow(text: String, isSelected: Boolean, onSelected: () -> Unit) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        RadioButton(
+            selected = isSelected,
+            onClick = onSelected,
+            modifier = Modifier.padding(vertical = 4.dp)
+        )
+        Text(text = text)
+    }
+}
+
+
+// Switch: allows users to toggle between two states, such as on/off or true/false.
+// Switch: designed specifically for binary on/off choices- it is a two-state control.
+@Composable
+fun SwitchExample(context: Context) {
+    // 1- Declare a mutable state variable to hold the checked state of the switch
+    // Tracking checked/unchecked state of the Switch
+    var isChecked by remember { mutableStateOf(false) }
+
+    // 2- Create a Switch composable and pass the checked state to it
+    Switch(
+        checked = isChecked,
+        onCheckedChange = {
+            isChecked = it
+            Toast.makeText(
+                context,
+                "Is Switch Checked: $isChecked",
+                Toast.LENGTH_SHORT
+            ).show()
+        },
+    )
+}
+
+// CheckBox: allows users to toggle between 2 states, allows users to select multiple options from a list of options.
+@Composable
+fun CheckBoxExample(context: Context) {
+    // 1- Declare a mutable state variable to hold the checked state of the checkbox
+    // Tracking checked/unchecked state of the checkbox
+    var checked by remember { mutableStateOf(false) }
+
+    // 2- Create a Checkbox composable and pass the checked state to it
+    Checkbox(
+        checked = checked,
+        onCheckedChange = { isChecked ->
+            checked = isChecked
+            Toast.makeText(
+                context,
+                "Is CheckBox Checked: $isChecked",
+                Toast.LENGTH_SHORT
+            ).show()
+        },
+    )
+}
+
+// Stateful Composable: Manages its own State
+@Composable
+fun StatefulTextField() {
+    var text by remember { mutableStateOf("") }
+    Column {
+        TextField(
+            value = text,
+            onValueChange = { newText ->
+                text = newText
+            },
+            label = { Text(text = "Enter Text") }
+        )
+        Text(text = "You Typed: $text")
+    }
+}
+
+// Stateless Composable: Stateful to Stateless Composable
+@Composable
+fun StatelessTextField(
+    text: String,
+    onValueChange: (String) -> Unit
+) {
+    Column {
+        TextField(
+            value = text,
+            onValueChange = onValueChange,
+            label = { Text(text = "Enter Text") }
+        )
+        Text(text = "You Typed: $text")
+    }
+}
+
+// Stateful Composable: Manages its own State
+@Composable
+fun StatefulCounter() {
+    var count by remember { mutableStateOf(0) }
+    Button(onClick = { count++ }) {
+        Text("Clicked: $count times")
+    }
+}
+
+// Stateless Composable
+@Composable
+fun StatelessCounter(count: Int, onClick: () -> Unit) {
+    var count by remember { mutableStateOf(0) }
+    Button(onClick = onClick) {
+        Text("Clicked: $count times")
+    }
+}
 
 // State Management
 /**
